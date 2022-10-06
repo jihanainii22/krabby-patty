b805260dced95e0b439101ce08aebd8f0b5517cb
jihanainii22 <jihanqurrotuainii08@gmail.com>
2022-10-06 11:33:16 +0700

    inisiasi resep krabby patty

---
+++ Resep.java
@@ -0,0 +1,8 @@
+public class Resep{
+    public static void main(String[] args){
+        System.out.println("Resep Krabby Patty");
+        System.out.println("-----------------");
+        System.out.println("1. Krabby Patty");
+        System.out.println("2. Bumbu");
+    }
+}
